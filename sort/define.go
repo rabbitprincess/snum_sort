fad647d29b673ab1edd46dbd7bd60d70450e0754
김창학 <sjc02183@naver.com>
2022-11-30 15:24:27 +0900

    Update define.go

--- sort/define.go
+++ sort/define.go
@@ -7,8 +7,8 @@ const (
 	DEF_digitIntegerMax int = 96
 	DEF_digitDecimalMax int = 32
 
-	DEF_headerBitMaskSign        byte = 0x80 // 1000 0000
-	DEF_headerBitMaskStandardLen byte = 0x7F // 0111 1111
+	DEF_headerBitMaskSign        byte = 0x80     // 1000 0000
+	DEF_headerBitMaskStandardLen byte = 0x80 - 1 // 0111 1111
 
-	DEF_asciiZeroUnderOne byte = 0x30 - 1 // 0011 0000
+	DEF_asciiZeroUnderOne byte = 0x30 - 1
 )
